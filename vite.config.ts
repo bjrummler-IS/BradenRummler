8d0c7f50deea871759fac02b6874e3158ea8ab5d
Braden Rummler <bradenrummler@Bradens-MacBook-Air-456.local>
2026-06-10 15:00:09 -0600

    Fix base path and add CNAME for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/BradenRummler/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
